3e8ee93e38f800642b8adf8ecfebd7b8c392daca
Eudy Cabral Burgos <eudy_cabral@ucne.edu.do>
2018-06-20 11:28:26 -0400

    Ventana de Articulos Lista

--- RegistroDetalle/UI/REGISTROS/Articulo.Designer.cs
+++ RegistroDetalle/UI/REGISTROS/Articulo.Designer.cs
@@ -28,19 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.Label fechaLabel;
+            System.Windows.Forms.Label cantidadLabel;
+            System.Windows.Forms.Label descripcionLabel;
+            System.Windows.Forms.Label nombreLabel;
+            System.Windows.Forms.Label articuloIdLabel;
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fechaDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.cantidadNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.descripcionTextBox = new System.Windows.Forms.TextBox();
+            this.nombreTextBox = new System.Windows.Forms.TextBox();
+            this.articuloIdNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            fechaLabel = new System.Windows.Forms.Label();
+            cantidadLabel = new System.Windows.Forms.Label();
+            descripcionLabel = new System.Windows.Forms.Label();
+            nombreLabel = new System.Windows.Forms.Label();
+            articuloIdLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cantidadNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.articuloIdNumericUpDown)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(351, 254);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 30;
+            this.button4.Text = "Eliminar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(216, 254);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 29;
+            this.button3.Text = "Guardar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(81, 254);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 28;
+            this.button2.Text = "Nuevo";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // fechaLabel
+            // 
+            fechaLabel.AutoSize = true;
+            fechaLabel.Location = new System.Drawing.Point(343, 37);
+            fechaLabel.Name = "fechaLabel";
+            fechaLabel.Size = new System.Drawing.Size(40, 13);
+            fechaLabel.TabIndex = 25;
+            fechaLabel.Text = "Fecha:";
+            // 
+            // fechaDateTimePicker
+            // 
+            this.fechaDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.fechaDateTimePicker.Location = new System.Drawing.Point(389, 33);
+            this.fechaDateTimePicker.Name = "fechaDateTimePicker";
+            this.fechaDateTimePicker.Size = new System.Drawing.Size(95, 20);
+            this.fechaDateTimePicker.TabIndex = 26;
+            // 
+            // cantidadLabel
+            // 
+            cantidadLabel.AutoSize = true;
+            cantidadLabel.Location = new System.Drawing.Point(36, 203);
+            cantidadLabel.Name = "cantidadLabel";
+            cantidadLabel.Size = new System.Drawing.Size(52, 13);
+            cantidadLabel.TabIndex = 23;
+            cantidadLabel.Text = "Cantidad:";
+            // 
+            // cantidadNumericUpDown
+            // 
+            this.cantidadNumericUpDown.Location = new System.Drawing.Point(103, 198);
+            this.cantidadNumericUpDown.Name = "cantidadNumericUpDown";
+            this.cantidadNumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.cantidadNumericUpDown.TabIndex = 24;
+            // 
+            // descripcionLabel
+            // 
+            descripcionLabel.AutoSize = true;
+            descripcionLabel.Location = new System.Drawing.Point(36, 131);
+            descripcionLabel.Name = "descripcionLabel";
+            descripcionLabel.Size = new System.Drawing.Size(66, 13);
+            descripcionLabel.TabIndex = 21;
+            descripcionLabel.Text = "Descripcion:";
+            // 
+            // descripcionTextBox
+            // 
+            this.descripcionTextBox.Location = new System.Drawing.Point(103, 128);
+            this.descripcionTextBox.Multiline = true;
+            this.descripcionTextBox.Name = "descripcionTextBox";
+            this.descripcionTextBox.Size = new System.Drawing.Size(120, 49);
+            this.descripcionTextBox.TabIndex = 22;
+            // 
+            // nombreLabel
+            // 
+            nombreLabel.AutoSize = true;
+            nombreLabel.Location = new System.Drawing.Point(36, 92);
+            nombreLabel.Name = "nombreLabel";
+            nombreLabel.Size = new System.Drawing.Size(47, 13);
+            nombreLabel.TabIndex = 19;
+            nombreLabel.Text = "Nombre:";
+            // 
+            // nombreTextBox
+            // 
+            this.nombreTextBox.Location = new System.Drawing.Point(103, 87);
+            this.nombreTextBox.Name = "nombreTextBox";
+            this.nombreTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nombreTextBox.TabIndex = 20;
+            // 
+            // articuloIdLabel
+            // 
+            articuloIdLabel.AutoSize = true;
+            articuloIdLabel.Location = new System.Drawing.Point(36, 35);
+            articuloIdLabel.Name = "articuloIdLabel";
+            articuloIdLabel.Size = new System.Drawing.Size(57, 13);
+            articuloIdLabel.TabIndex = 17;
+            articuloIdLabel.Text = "Articulo Id:";
+            // 
+            // articuloIdNumericUpDown
+            // 
+            this.articuloIdNumericUpDown.Location = new System.Drawing.Point(103, 33);
+            this.articuloIdNumericUpDown.Name = "articuloIdNumericUpDown";
+            this.articuloIdNumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.articuloIdNumericUpDown.TabIndex = 18;
             // 
             // Articulo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(520, 308);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(fechaLabel);
+            this.Controls.Add(this.fechaDateTimePicker);
+            this.Controls.Add(cantidadLabel);
+            this.Controls.Add(this.cantidadNumericUpDown);
+            this.Controls.Add(descripcionLabel);
+            this.Controls.Add(this.descripcionTextBox);
+            this.Controls.Add(nombreLabel);
+            this.Controls.Add(this.nombreTextBox);
+            this.Controls.Add(articuloIdLabel);
+            this.Controls.Add(this.articuloIdNumericUpDown);
             this.Name = "Articulo";
             this.Text = "Articulo";
+            ((System.ComponentModel.ISupportInitialize)(this.cantidadNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.articuloIdNumericUpDown)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker fechaDateTimePicker;
+        private System.Windows.Forms.NumericUpDown cantidadNumericUpDown;
+        private System.Windows.Forms.TextBox descripcionTextBox;
+        private System.Windows.Forms.TextBox nombreTextBox;
+        private System.Windows.Forms.NumericUpDown articuloIdNumericUpDown;
     }
 }
