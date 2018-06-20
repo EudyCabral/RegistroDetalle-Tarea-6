25a6d0c2dd5dffcef8d928d84aa6db15fe7d74d0
Eudy Cabral Burgos <eudy_cabral@ucne.edu.do>
2018-06-20 10:38:38 -0400

    Clase Persona Lista Clase Articulos Lista

---
+++ RegistroDetalle/ENTIDADES/Articulos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace RegistroDetalle.ENTIDADES
+{
+    public class Articulos
+    {
+        [Key]
+        public int ArticuloId { get; set; }
+        public DateTime dateTime { get; set; }
+        public string Nombre { get; set; }
+     
+
+        public Articulos()
+        {
+            ArticuloId = 0;
+            this.dateTime = DateTime.Now;
+            Nombre =string.Empty;
+          
+        }
+    }
+}
